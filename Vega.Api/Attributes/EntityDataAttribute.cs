255364e684257008e7861e6df235159ca25245f1
Tommaso Giachi <tom@isendu.com>
2023-06-19 18:26:45 +0200

    feat(.editorconfig): add .editorconfig file with rules for indentation, line length, and whitespace to ensure consistent code style across the project
    chore: add ReSharper inspection severities and enforce consistent line endings and indentation size in relevant files
    
    feat(gitignore): add .gitignore file with C# and IntelliJ+all templates from toptal.com/developers/gitignore
    
    This commit adds a .gitignore file to the repository, which includes templates for C# and IntelliJ+all from toptal.com/developers/gitignore. This file will help to ignore unnecessary files and directories generated by the development environment and build process.
    
    chore: add IntelliJ IDEA project files to .gitignore to avoid committing IDE-specific files
    
    feat: add MIT License to the project
    feat: add SECURITY.md file to the project
    feat: add EntityDataAttribute class to Vega.Api project
    feat: add Vega.Api project to VegaRL solution
    feat: set TargetFramework to net7.0 in Vega.Api project
    feat: add Data\Entities folder to Vega.Api project

---
+++ Vega.Api/Attributes/EntityDataAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Vega.Api.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class EntityDataAttribute : Attribute
+{
+    public string TypeName { get; set; }
+
+    public EntityDataAttribute(string typeName)
+    {
+        TypeName = typeName;
+    }
+}
