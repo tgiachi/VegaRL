d488079812e4c087fed139a5c34099224cc429f1
tgiachi <squid@stormwind.it>
2023-06-25 22:19:40 +0200

    feat(ITickService.cs, TickService.cs): add TickService and ITickService interfaces
    Add ITickService interface and TickService implementation to the Vega.Engine project. The TickService class is a service that provides a method to parse a string into a TimeSpan. The ITickService interface inherits from the IVegaService interface.

---
+++ Vega.Engine/Services/TickService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Logging;
+using Vega.Engine.Interfaces;
+using Vega.Engine.Services.Base;
+
+namespace Vega.Engine.Services;
+
+public class TickService : BaseVegaService<ITickService>, ITickService
+{
+    public TickService(ILogger<ITickService> logger) : base(logger)
+    {
+    }
+
+    /// <summary>
+    ///  Parses a string into a TimeSpan.
+    ///  Example: 1d 2h 3m 4s
+    /// </summary>
+    /// <param name="timeSpanString"></param>
+    /// <returns></returns>
+    private TimeSpan ParseTimeSpanFromString(string timeSpanString)
+    {
+        var days = 0;
+        var hours = 0;
+        var minutes = 0;
+        var seconds = 0;
+
+        var parts = timeSpanString.Split(' ');
+
+        foreach (string part in parts)
+        {
+            if (part.EndsWith("d"))
+            {
+                days = int.Parse(part.TrimEnd('d'));
+            }
+            else if (part.EndsWith("h"))
+            {
+                hours = int.Parse(part.TrimEnd('h'));
+            }
+            else if (part.EndsWith("m"))
+            {
+                minutes = int.Parse(part.TrimEnd('m'));
+            }
+            else if (part.EndsWith("s"))
+            {
+                seconds = int.Parse(part.TrimEnd('s'));
+            }
+        }
+
+        var timeSpan = new TimeSpan(days, hours, minutes, seconds);
+
+        return timeSpan;
+    }
+}
