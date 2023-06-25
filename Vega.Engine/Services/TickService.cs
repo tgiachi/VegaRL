295dc0e8506a2cd634d9e66708e39757bf0e24e0
tgiachi <squid@stormwind.it>
2023-06-25 22:22:03 +0200

    refactor(TickService.cs): extract regex pattern to a static field for better readability and reusability feat(TickService.cs): add regex matching to ParseTimeSpanFromString method to extract values of days, hours, minutes, and seconds from input string and create a TimeSpan object using the extracted values

--- Vega.Engine/Services/TickService.cs
+++ Vega.Engine/Services/TickService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Text.RegularExpressions;
+using Microsoft.Extensions.Logging;
 using Vega.Engine.Interfaces;
 using Vega.Engine.Services.Base;
 
@@ -6,6 +7,11 @@ namespace Vega.Engine.Services;
 
 public class TickService : BaseVegaService<ITickService>, ITickService
 {
+    private static string _pattern = @"(?:(\d+)d\s*)?(?:(\d+)h\s*)?(?:(\d+)m\s*)?(?:(\d+)s)?";
+
+    // Execute the regex on the input string
+    //Match match = Regex.Match(input, pattern);
+
     public TickService(ILogger<ITickService> logger) : base(logger)
     {
     }
@@ -18,35 +24,33 @@ public class TickService : BaseVegaService<ITickService>, ITickService
     /// <returns></returns>
     private TimeSpan ParseTimeSpanFromString(string timeSpanString)
     {
-        var days = 0;
-        var hours = 0;
-        var minutes = 0;
-        var seconds = 0;
+        // Define the regex pattern to match the values of days, hours, minutes, and seconds
 
-        var parts = timeSpanString.Split(' ');
+        int days = 0;
+        int hours = 0;
+        int minutes = 0;
+        int seconds = 0;
 
-        foreach (string part in parts)
+        // Extract the values from the regex matches
+        if (match.Success)
         {
-            if (part.EndsWith("d"))
-            {
-                days = int.Parse(part.TrimEnd('d'));
-            }
-            else if (part.EndsWith("h"))
-            {
-                hours = int.Parse(part.TrimEnd('h'));
-            }
-            else if (part.EndsWith("m"))
-            {
-                minutes = int.Parse(part.TrimEnd('m'));
-            }
-            else if (part.EndsWith("s"))
-            {
-                seconds = int.Parse(part.TrimEnd('s'));
-            }
+            if (match.Groups[1].Success)
+                days = int.Parse(match.Groups[1].Value);
+
+            if (match.Groups[2].Success)
+                hours = int.Parse(match.Groups[2].Value);
+
+            if (match.Groups[3].Success)
+                minutes = int.Parse(match.Groups[3].Value);
+
+            if (match.Groups[4].Success)
+                seconds = int.Parse(match.Groups[4].Value);
         }
 
-        var timeSpan = new TimeSpan(days, hours, minutes, seconds);
+        // Create a TimeSpan object using the extracted values
+        TimeSpan timeSpan = new TimeSpan(days, hours, minutes, seconds);
 
         return timeSpan;
+    }
     }
 }
