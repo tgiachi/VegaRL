4aed4e96f9cbcf73e10abb6065902c258c7aab56
tgiachi <squid@stormwind.it>
2023-06-26 07:46:17 +0200

    refactor(TickService.cs): make _pattern and _regex fields readonly for better performance and thread safety feat(TickService.cs): add comments to improve code readability and maintainability

--- Vega.Engine/Services/TickService.cs
+++ Vega.Engine/Services/TickService.cs
@@ -7,7 +7,8 @@ namespace Vega.Engine.Services;
 
 public class TickService : BaseVegaService<ITickService>, ITickService
 {
-    private static string _pattern = @"(?:(\d+)d\s*)?(?:(\d+)h\s*)?(?:(\d+)m\s*)?(?:(\d+)s)?";
+    private static readonly string _pattern = @"(?:(\d+)d\s*)?(?:(\d+)h\s*)?(?:(\d+)m\s*)?(?:(\d+)s)?";
+    private readonly Regex _regex = new(_pattern, RegexOptions.Compiled);
 
     // Execute the regex on the input string
     //Match match = Regex.Match(input, pattern);
@@ -31,26 +32,34 @@ public class TickService : BaseVegaService<ITickService>, ITickService
         int minutes = 0;
         int seconds = 0;
 
+        var match = _regex.Match(timeSpanString);
         // Extract the values from the regex matches
         if (match.Success)
         {
             if (match.Groups[1].Success)
+            {
                 days = int.Parse(match.Groups[1].Value);
+            }
 
             if (match.Groups[2].Success)
+            {
                 hours = int.Parse(match.Groups[2].Value);
+            }
 
             if (match.Groups[3].Success)
+            {
                 minutes = int.Parse(match.Groups[3].Value);
+            }
 
             if (match.Groups[4].Success)
+            {
                 seconds = int.Parse(match.Groups[4].Value);
+            }
         }
 
         // Create a TimeSpan object using the extracted values
-        TimeSpan timeSpan = new TimeSpan(days, hours, minutes, seconds);
+        var timeSpan = new TimeSpan(days, hours, minutes, seconds);
 
         return timeSpan;
-    }
     }
 }
